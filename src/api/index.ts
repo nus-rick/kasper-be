227252ba419fd9f6a5c417c920d59b5fd55aa527
nusrick <rick@nustechnology.com>
2022-06-09 10:38:22 +0700

    Update cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,12 +11,12 @@ class Api implements ApiInterface {
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+    app.use(cors());
     app.use('/api/v1', baseRouter.routes);
 
     app.get('/', (_req: Request, res: Response) => {
       res.send('Welcome to NUS express application!');
     });
-    app.use(cors());
     return app;
   }
 }
